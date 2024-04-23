dc5cb3e360df771f9ab7f823c2bf677a7d083179
Ario Ostovary <arioostov@gmail.com>
2024-04-23 12:45:24 -0400

    first commit

---
+++ arcade.py
@@ -0,0 +1,13 @@
+from pygame import *
+
+init()
+size = width, height = 800, 600
+screen = display.set_mode(size)
+display.set_caption("My Game")
+my_clock = time.Clock()
+running = True
+
+
+class Game:
+    pass
+    pass
---
+++ main.py
@@ -0,0 +1,4 @@
+from arcade import Game
+
+if __name__ == "__main__":
+    Game()
