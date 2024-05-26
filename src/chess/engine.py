6bfcc246bc6f8ceb9992b98f34fc9ef9edd8cc35
Ario Ostovary <arioostov@gmail.com>
2024-05-26 18:26:44 -0400

    engine needs work

--- src/chess/engine.py
+++ src/chess/engine.py
@@ -1,72 +1,70 @@
-from src.chess.board import Board, Piece
-import src.chess.PSQT as PSQT
-from random import uniform
-
+# from src.chess.engine.PSQT import PSQT
 import time
-
-# import PSQT
-# from board import Board, Piece
-
-# a positive evaluation of a position means white is winning
-# a negative evaluation of a position means black is winning
-
-PIECE_VALUES = {
-    Piece.white | Piece.pawn: 1,
-    Piece.white | Piece.knight: 3,
-    Piece.white | Piece.bishop: 3,
-    Piece.white | Piece.rook: 5,
-    Piece.white | Piece.queen: 9,
-    Piece.white | Piece.king: 100,
-
-    Piece.black | Piece.pawn: -1,
-    Piece.black | Piece.knight: -3,
-    Piece.black | Piece.bishop: -3,
-    Piece.black | Piece.rook: -5,
-    Piece.black | Piece.queen: -9,
-    Piece.black | Piece.king: -100
-}
-
-# Evaluation factors
-MOBILITY_FACTOR = 0.2
-CHECK_SCORE = 0.75
-DOUBLE_PAWNS_DISADVANTAGE = -0.75
-ISOLATED_PAWNS_DISADVANTAGE = -0.6 # down from -0.75
-BACKWARD_PAWNS_DISADVANTAGE = -0.5
-PASSED_PAWNS_ADVANTAGE = 1.2
-CENTER_CONTROL_ADVANTAGE = 0.3 # | down from 0.4
-# DEFENDING_ALLY_PIECES_ADVANTAGE = 0.5 | my generate move function doesn't create moves that defend pieces TODO: implement this
-ATTACKING_ENEMY_PIECES_ADVANTAGE = 0.35 # | down from 0.5
-KING_SAFETY_ADVANTAGE = 0.5
-DEVELOPMENT_ADVANTAGE = 0.5
+from src.chess.PSQT import PSQT, PHASE_WEIGHTS, TOTAL_PHASE
+from src.chess.board import Board
 
 
-LEGAL_SQUARES = [i for i in range(127) if not i & 0x88]
-CENTRAL_LEGAL_SQUARES = [i for i in LEGAL_SQUARES if 3 <= i // 16 <= 6]
-CENTER_SQUARES = [51, 52, 67, 68]
 
-POSITIVE_INFINITY = float('inf')
-NEGATIVE_INFINITY = float('-inf')
+def calculate_phase(board):
+    phase = 0
 
-FILE_MASKS = [0x0101010101010101 << i for i in range(8)]
+    for square in board:
+        if square != 0:
+            phase += PHASE_WEIGHTS[square & 7]
 
-piece_map = {
-    0b1001: PSQT.white_pawn,
-    0b1010: PSQT.white_knight,
-    0b1011: PSQT.white_bishop,
-    0b1100: PSQT.white_rook,
-    0b1101: PSQT.white_queen,
-    0b1110: PSQT.white_king,
+    return phase / TOTAL_PHASE # this is a float between 0 and 1
 
-    0b0001: PSQT.black_pawn,
-    0b0010: PSQT.black_knight,
-    0b0011: PSQT.black_bishop,
-    0b0100: PSQT.black_rook,
-    0b0101: PSQT.black_queen,
-    0b0110: PSQT.black_king
-}
+def get_piece_square_table_value(piece, square, phase):
+
+    opening_table = PSQT["Opening"][piece]
+    endgame_table = PSQT["Endgame"][piece]
+
+    rank = square // 8
+    file = square % 8
+
+    opening_value = opening_table[7 - rank][file]
+    endgame_value = endgame_table[7 - rank][file]
+
+    return opening_value * (1 - phase) + endgame_value * phase
+
+def evaluate_psqt(board: Board):
+    phase = calculate_phase(board.board)
+
+    score = 0
+
+    # TODO: switch to using board.white_pieces and board.black_pieces
+    for square, piece in enumerate(board.board):
+        if piece != 0:
+            score += get_piece_square_table_value(piece, square, phase)
+
+    return score
+
+
+
+
+
+
+
+
+
+def evaluate(board: Board):
+    evaluation = 0
+    
+    # Material
+    evaluation += evaluate_psqt(board)
+
+    # TODO: add more evaluation terms
+
+    return evaluation
+
+
+
+
+NEGATIVE_INFINITY = -float("inf")
+POSITIVE_INFINITY = float("inf")
 
 class Engine:
-    def __init__(self, board: Board, depth: int = 20, time_limit_ms: int = 5000):
+    def __init__(self, board: Board, depth: int = 6, time_limit_ms: int = 2500):
         self.board = board.__copy__()
         self.depth = depth
         self.time_limit_ms = time_limit_ms
@@ -93,190 +91,83 @@ class Engine:
         self.board.fullmove_number = board.fullmove_number
         self.board.undo_list = board.undo_list.copy()
 
-    def evaluate_piece_values(self):
-        piece_value_evaluation = 0
-        for square in self.board.white_pieces + self.board.black_pieces:
-            piece_value_evaluation += PIECE_VALUES[self.board.get_piece(square)]
 
-        return piece_value_evaluation
-        
-    def evaluate_board(self):
-        key = self.board.hash_board(self.board.white_to_move)
-        if key in self.evaluated_boards:
-            return self.evaluated_boards[key]
-
-        if self.board.is_checkmate(True):
-            self.evaluated_boards[key] = NEGATIVE_INFINITY
-            return NEGATIVE_INFINITY
-        
-        if self.board.is_checkmate(False):
-            self.evaluated_boards[key] = POSITIVE_INFINITY
-            return POSITIVE_INFINITY
-        
-        evaluation = 0
-        evaluation += self.evaluate_piece_values()
-        # evaluation += self.evaluate_double_pawns()
-        # evaluation += self.evaluate_isolated_pawns()
-        # evaluation += self.evaluate_mobility() # | VERY SLOW
-        # evaluation += self.evaluate_check()
-        # evaluation += self.evaluate_center_control()
-        # evaluation += self.evaluate_attack() # | VERY SLOW
-        # evaluation += self.evaluate_piece_square_tables()
-        # evaluation += self.evaluate_development()
-
-        # add some randomness to the evaluation
-        evaluation += uniform(-0.01, 0.01) 
-
-        self.evaluated_boards[key] = evaluation
-        return evaluation
 
     def time_exceeded(self):
         return (time.time() - self.start_time) * 1000 >= self.time_limit_ms
     
-    def quiescence_search(self, alpha, beta):
-        stand_pat = self.evaluate_board()
-        if stand_pat >= beta:
-            return beta
-        if alpha < stand_pat:
-            alpha = stand_pat
-
-        for move in self.board.generate_legal_moves(self.board.white_to_move):
-            if move[3]:
-                self.board.make_move(move)
-                score = -self.quiescence_search(-beta, -alpha)
-                self.board.undo_move()
-
-                if score >= beta:
-                    return beta
-                if score > alpha:
-                    alpha = score
-
-        return alpha
+    def quiescence_search(self):
+        # TODO: implement quiescence search
+        return evaluate(self.board)
 
     def minimax(self, depth, alpha, beta):
-        if depth == 0 or self.board.is_game_over() or self.time_exceeded():
-            return self.quiescence_search(alpha, beta)
-        
+        # TODO: review this code
+        if depth == 0 or self.time_exceeded():
+            return self.quiescence_search()
+
         if self.board.white_to_move:
             best_eval = NEGATIVE_INFINITY
             for move in self.board.generate_legal_moves(True):
                 self.board.make_move(move)
-                eval = self.minimax(depth - 1, alpha, beta)
+                value = self.minimax(depth - 1, alpha, beta)
                 self.board.undo_move()
-                best_eval = max(best_eval, eval)
-                alpha = max(alpha, best_eval)
-                if beta <= alpha or self.time_exceeded():
+                best_eval = max(best_eval, value)
+                alpha = max(alpha, value)
+                if beta <= alpha:
                     break
 
         else:
             best_eval = POSITIVE_INFINITY
             for move in self.board.generate_legal_moves(False):
                 self.board.make_move(move)
-                eval = self.minimax(depth - 1, alpha, beta)
+                value = self.minimax(depth - 1, alpha, beta)
                 self.board.undo_move()
-                best_eval = min(best_eval, eval)
-                beta = min(beta, best_eval)
-                if beta <= alpha or self.time_exceeded():
+                best_eval = min(best_eval, value)
+                beta = min(beta, value)
+                if beta <= alpha:
                     break
 
         return best_eval
-
+    
     def find_best_move(self, result_container):
-        try:
-            self.start_time = time.time()
-            starting_player = self.board.white_to_move
-            best_move = None
-            best_eval = NEGATIVE_INFINITY if starting_player else POSITIVE_INFINITY
+        # TODO: check if this code is correct
+        self.start_time = time.time()
+        starting_player = self.board.white_to_move
+        best_move = None
+        best_eval = NEGATIVE_INFINITY if starting_player else POSITIVE_INFINITY
 
-            alpha = NEGATIVE_INFINITY
-            beta = POSITIVE_INFINITY
+        alpha = NEGATIVE_INFINITY
+        beta = POSITIVE_INFINITY
 
-            for current_depth in range(1, self.depth + 1):
-                if self.time_exceeded():
-                    break
-                
-                current_best_move = None
-                current_best_eval = NEGATIVE_INFINITY if starting_player else POSITIVE_INFINITY
+        for current_depth in range(1, self.depth + 1):
 
-                if starting_player: # white 
-                    for move in self.board.generate_legal_moves(True):
-                        self.board.make_move(move)
-                        eval = self.minimax(current_depth - 1, alpha, beta)
-                        self.board.undo_move()
-                        if eval > current_best_eval:
-                            current_best_eval = eval
-                            current_best_move = move
-                        alpha = max(alpha, current_best_eval)
+            if starting_player:
+                for move in self.board.generate_legal_moves(True):
+                    self.board.make_move(move)
+                    value = self.minimax(current_depth - 1, alpha, beta)
+                    self.board.undo_move()
+                    if value > best_eval:
+                        best_eval = value
+                        best_move = move
+                    alpha = max(alpha, value)
+                    if beta <= alpha:
+                        break
 
-                else: # black
-                    for move in self.board.generate_legal_moves(False):
-                        self.board.make_move(move)
-                        eval = self.minimax(current_depth - 1, alpha, beta)
-                        self.board.undo_move()
-                        if eval < current_best_eval:
-                            current_best_eval = eval
-                            current_best_move = move
-                        beta = min(beta, current_best_eval)
-                
-                if not self.time_exceeded():
-                    best_move = current_best_move
-                    best_eval = current_best_eval
-                    print(f'Best move at depth {current_depth}: {best_move} with evaluation {best_eval}')
-                    result_container.append((best_move, best_eval, False)) # false means the search was not completed and the move should not be made
+            else:
+                for move in self.board.generate_legal_moves(False):
+                    self.board.make_move(move)
+                    value = self.minimax(current_depth - 1, alpha, beta)
+                    self.board.undo_move()
+                    if value < best_eval:
+                        best_eval = value
+                        best_move = move
+                    beta = min(beta, value)
+                    if beta <= alpha:
+                        break
 
-            print(f'Best move: {best_move} with evaluation {best_eval}')
-            result_container.append((best_move, best_eval, True)) # true means the search was completed and the move can be made
-        except Exception as e:
-            print(e)
-            print(self.board.create_fen())
+            result_container.append((best_move, best_eval, False))
+            print(f"Depth: {current_depth}, Best move: {best_move}, Best eval: {best_eval}")
 
 
-if __name__ == '__main__':
-    import cProfile
-    import pstats
-    import csv
-
-    board = Board()
-    # board.load_fen('rnbqkbnr/ppppp2p/5p2/6p1/4PP2/8/PPPP2PP/RNBQKBNR w - - 0 1')
-    # board.make_move(encode_move(3, 71, 0b1101))
-    # board.load_fen('rnbqkbnr/ppppp2p/5p2/6pQ/4PP2/8/PPPP2PP/RNB1KBNR b - - 0 1')
-    engine = Engine(board, depth=4)
-
-    # engine.find_best_move()
-    container = []
-
-    profiler = cProfile.Profile()
-    profiler.enable()
-    engine.find_best_move(container)
-    # print(engine.evaluate_board())
-    # print(board.is_game_over())
-    profiler.disable()
-    print(container)
-
-    stats = pstats.Stats(profiler)
-
-    stats.sort_stats(pstats.SortKey.CUMULATIVE)
-
-    profiling_data = []
-    stats_data = stats.stats
-    for func, (cc, nc, tt, ct, callers) in stats_data.items():
-        filename, lineno, funcname = func
-        profiling_data.append({
-            'filename': filename,
-            'line_number': lineno,
-            'function_name': funcname,
-            'call_count': cc,
-            'recursive_call_count': nc,
-            'total_time': tt,
-            'cumulative_time': ct
-        })
-
-    csv_filename = 'profiling_results.csv'
-
-    with open(csv_filename, mode='w', newline='') as csv_file:
-        fieldnames = ['filename', 'line_number', 'function_name', 'call_count', 'recursive_call_count', 'total_time', 'cumulative_time']
-        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-
-        writer.writeheader()
-        for data in profiling_data:
-            writer.writerow(data)
+        result_container.append((best_move, best_eval, True))
+        print(f"Best move: {best_move}, Best eval: {best_eval}")
