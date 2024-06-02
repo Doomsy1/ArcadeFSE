26eea3d43491f7924aafd6681da9d562067bb250
Ario Ostovary <arioostov@gmail.com>
2024-06-01 22:09:11 -0400

    adjusted psqt values

--- src/chess/PSQT.py
+++ src/chess/PSQT.py
@@ -1,16 +1,31 @@
 from src.chess.PSQT_2D import PSQT_2D
 from src.chess.board import Piece
 
+piece_values = {
+    Piece.pawn: 100,
+    Piece.knight: 290,
+    Piece.bishop: 320,
+    Piece.rook: 500,
+    Piece.queen: 900,
+    Piece.king: 0
+}
+
 PSQT = {}
 
 for phase in PSQT_2D:
     PSQT[phase] = {}
     for piece in PSQT_2D[phase]:
+        piece_type = Piece.get_type(piece)
+        piece_value = piece_values[piece_type]
+        piece_value *= -1 if Piece.get_color(piece) == Piece.black else 1
+        
         table = PSQT_2D[phase][piece]
 
         table_1d = []
         # convert the 2d list into a tuple
         for rank in table[::-1]:
+            # add piece values to the rank
+            rank = [piece_value + square for square in rank]
             table_1d.extend(rank)
 
         table_tuple = tuple(table_1d)
@@ -27,10 +42,10 @@ PHASE_WEIGHTS = {
 }
 
 TOTAL_PHASE = (
-    8 * PHASE_WEIGHTS[Piece.pawn] +
-    2 * PHASE_WEIGHTS[Piece.knight] +
-    2 * PHASE_WEIGHTS[Piece.bishop] +
-    2 * PHASE_WEIGHTS[Piece.rook] +
-    1 * PHASE_WEIGHTS[Piece.queen] +
-    1 * PHASE_WEIGHTS[Piece.king]
+    16 * PHASE_WEIGHTS[Piece.pawn] +
+    4 * PHASE_WEIGHTS[Piece.knight] +
+    4 * PHASE_WEIGHTS[Piece.bishop] +
+    4 * PHASE_WEIGHTS[Piece.rook] +
+    2 * PHASE_WEIGHTS[Piece.queen] +
+    2 * PHASE_WEIGHTS[Piece.king]
 )
