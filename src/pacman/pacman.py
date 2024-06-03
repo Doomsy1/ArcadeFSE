2a532dd946aea0393090d6148d67c3b4cd8c63cd
Ario Ostovary <arioostov@gmail.com>
2024-06-03 01:14:53 -0400

    started on pacman

--- src/pacman/pacman.py
+++ src/pacman/pacman.py
@@ -1,4 +1,5 @@
 import pygame
+from src.pacman.map import PacmanMap
 from constants import PACMAN_VEL
 
 # Constants
@@ -9,12 +10,24 @@ class PacmanGame:
         self.screen = screen
         self.clock = pygame.time.Clock()
 
+        self.map = PacmanMap(screen)
+
+
     def main_loop(self):
         running = True
         while running:
-            pygame.display.flip()
+            # handle events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    return "main menu"
+                # escape key
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                    return "main menu"
+
+            self.map.draw()
 
             # set the caption as the fps
             pygame.display.set_caption(f"Pacman | FPS: {int(self.clock.get_fps())}")
             self.clock.tick(FPS)
+            pygame.display.flip()
         return "exit"
